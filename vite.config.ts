3504c2ceeed46609229e4c69a08cc105a3d6f002
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2024-12-23 23:32:28 +0700

    rerun build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import fs from 'fs'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig(({ command, mode }) => {
   const isDevelopment = mode === 'development'
 
