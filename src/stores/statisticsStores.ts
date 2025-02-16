e0d97eb5a8c3e5d645865b292657c4e756eba45c
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2025-02-16 19:11:28 +0700

    feature/41: добавил summary

--- src/stores/statisticsStores.ts
+++ src/stores/statisticsStores.ts
@@ -1,10 +1,11 @@
 // stores/userStore.ts
 import { defineStore } from 'pinia'
 import { makeRequest } from '@/utils/makeRequest'
-import type { Statistic } from '@/types/statistic'
+import type { Statistic, StatisticsSummaryResponse } from '@/types/statistic'
 
 interface StatisticsState {
   statistics: Statistic[]
+  statisticsSummary: StatisticsSummaryResponse[]
   loading: boolean
   error: string | null
 }
@@ -12,6 +13,7 @@ interface StatisticsState {
 export const useStatisticsStores = defineStore('statistics', {
   state: (): StatisticsState => ({
     statistics: [],
+    statisticsSummary: [],
     loading: false,
     error: null,
   }),
@@ -35,6 +37,38 @@ export const useStatisticsStores = defineStore('statistics', {
         this.loading = false
       }
     },
+    async fetchStatisticsSummary(type: string, year: number) {
+      this.loading = true
+      this.error = null
+      try {
+        const response = await makeRequest<StatisticsSummaryResponse>(
+          `analitique/get-analitique-summary?type=${type}&year=${year}`,
+          'get',
+        )
+
+        const existingRecord = this.statisticsSummary.find((item) => item.type === type);
+
+        if (!existingRecord) {
+          // Если записи с таким type нет, добавляем новую запись
+          this.statisticsSummary.push({
+            ...response,
+            type: type,
+          });
+        } else {
+          // Если запись существует, обновляем её
+          Object.assign(existingRecord, { ...response, type: type });
+        }
+
+
+      } catch (error) {
+        this.error = error instanceof Error ? error.message : 'Failed to fetch orders'
+      } finally {
+        this.loading = false
+      }
+    },
+    getStatisticsSummary(type: string) {
+      return this.statisticsSummary?.find((item) => item.type === type)
+    }
   },
 })
 
