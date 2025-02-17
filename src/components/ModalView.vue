e168a6870e6d122f296684ac8b76189f509d9f2f
Огнев Юрий Юрьевич <yu.ognev@s7.ru>
2025-02-17 23:58:14 +0700

    update libs

--- src/components/ModalView.vue
+++ src/components/ModalView.vue
@@ -5,7 +5,7 @@
         <button @click="emitClose" class="close-btn" aria-label="Close modal">
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M18 6L6 18M6 6L18 18" stroke="black" stroke-width="2" stroke-linecap="round"
-              stroke-linejoin="round" />
+                  stroke-linejoin="round" />
           </svg>
 
         </button>
@@ -24,7 +24,7 @@ interface Props {
   show: boolean;
 }
 
-const props = defineProps<Props>();
+defineProps<Props>();
 
 const emit = defineEmits<{
   (e: 'close-modal'): void
